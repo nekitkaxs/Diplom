0dd98b4f5c7f1e354f6402d6a8ab130d8bd14f94
Nikita <nekitkaxs8@gmail.com>
2025-03-04 14:01:18 +0400

    Diplom

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://64f9b016-f326-4efa-a1f8-9840c9ee33a4.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://d1b5fdb0-9707-45f6-bb90-500d5b7ca0ff.serverhub.praktikum-services.ru"
 CREATE_ORDERS = "/api/v1/orders"
 ORDER_TRACK = "/api/v1/orders/track"
--- test_scooter.py
+++ test_scooter.py
@@ -1,19 +1,14 @@
 import sender_stand_request
 import data
 
-# Тестовый сценарий для создания и получения заказа
 def test_create_and_fetch_order():
-    # Создание заказа
     create_response = sender_stand_request.post_new_order(data.order_body)
     assert create_response.status_code == 201, "Не удалось создать заказ."
     
-    # Сохранение трек-номера заказа
     track_number = create_response.json().get("track")
     assert track_number, "Неверный ответ сервера. Трек-номер не получен."
     
-    # Получение заказа по трек-номеру
     get_response = sender_stand_request.get_order_track(track_number)
     assert get_response.status_code == 200, "Не удалось получить заказ."
     
-    # Вывод успешного результата
     print("Тест пройден. Заказ успешно создан и получен.")
